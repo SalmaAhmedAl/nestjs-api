c59329208fc44fa330330a4ccbe8d3511a53a68c
SalmaAhmedAl <salmahamzaahmed77@gmail.com>
2024-03-17 22:54:57 +0200

    global pipe

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
-import { UserModule } from './user/user.module';
+import { AuthModule } from './auth/auth.module';
 import { BookmarkModule } from './bookmark/bookmark.module';
+import { UserModule } from './user/user.module';
+import { PrismaModule } from './prisma/prisma.module';
 
 
 @Module({
-  imports: [UserModule, BookmarkModule],
+  imports: [AuthModule, BookmarkModule, UserModule, PrismaModule],
   controllers: [],
   providers: [],
 })
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,14 +1,18 @@
 import { Injectable } from "@nestjs/common";
+import { PrismaService } from "src/prisma/prisma.service";
 
-@Injectable({})
+@Injectable()
 export class AuthService{
+    constructor(private prisma: PrismaService){
 
+    }
     signup(){
-
+        
+        return {msg: "I am signing up"};
     }
 
     signin(){
-        
+        return {msg: "I am signing in"};
     }
 
 }
