c7f4f0b1d9cff7f26ec6effb89645a5420e32079
SalmaAhmedAl <salmahamzaahmed77@gmail.com>
2024-03-18 23:44:46 +0200

    AuthGuard added

--- src/auth/strategy/jwt.strategy.ts
+++ src/auth/strategy/jwt.strategy.ts
@@ -1,18 +1,31 @@
 import { Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import {PassportStrategy} from '@nestjs/passport'
+import { Console } from 'console';
 import { Strategy , ExtractJwt} from 'passport-jwt';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
-export class JwtStrategy extends PassportStrategy(Strategy) {
+export class JwtStrategy extends PassportStrategy(Strategy, 'jwt') {
 //this class to validate the access token
-constructor(config: ConfigService){
+constructor(config: ConfigService, private prisma:PrismaService){
     super({
         jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
         secretOrKey: config.get('JWT_SECRET')
     })
 }
-
+  async validate(payload: {sub:number, email:string}){
+    console.log({
+        payload
+    });
+    const user = await this.prisma.user.findUnique({
+        where: {
+            id: payload.sub
+        }
+    });
+    delete user.hash;
+    return user; 
+   }
 }
 //We can protect some of our routes with this strategy
 //Only people with a valid token can access it
--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,12 @@
-import { Controller } from '@nestjs/common';
-
-@Controller('user')
-export class UserController {}
+import { Controller, Get, Req, UseGuards } from '@nestjs/common';
+import { Request } from 'express';
+import { JwtGuard } from 'src/auth/guard';
+//We can use guard global at the controller level or at the route level
+@Controller('users')
+export class UserController {
+    @UseGuards(JwtGuard)
+    @Get('me')
+    getMe(@Req() req:Request){
+        return req.user;
+    }
+}
