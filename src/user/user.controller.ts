80abba88297a3de866eafcec7f57be11bfbff7f2
SalmaAhmedAl <salmahamzaahmed77@gmail.com>
2024-03-19 05:14:37 +0200

    editUser

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,13 +1,24 @@
-import { Controller, Get, Req, UseGuards } from '@nestjs/common';
+import { Body, Controller, Get, Patch, Req, UseGuards } from '@nestjs/common';
 import { User } from '@prisma/client';
 import { GetUser } from '../auth/decorator';
 import { JwtGuard } from '../auth/guard';
+import { UserService } from './user.service';
+import { EditUserDto } from './dto/edit-user.dto';
 //We can use guard global at the controller level or at the route level
 @Controller('users')
 export class UserController {
+    constructor(private userService: UserService) {}
     @UseGuards(JwtGuard)
     @Get('me')
     getMe(@GetUser() user: User){
         return user;
     }
+
+    @Patch()
+    editUser(
+    @GetUser('id') userId: number,
+    @Body() dto: EditUserDto,
+  ) {
+    return this.userService.editUser(userId, dto);
+  }
 }
